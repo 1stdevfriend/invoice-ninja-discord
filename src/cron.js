478220c0530e6d780835d1abe09f852a54e6be80
Kartikey Kushwah <support@koders.in>
2025-06-17 15:29:09 +0530

    feat: add daily recurring invoice check with Discord embed

--- src/cron.js
+++ src/cron.js
@@ -1,5 +1,6 @@
 const cron = require('node-cron');
 const { sendYearlySummaryToDiscord } = require('./utils');
+const { checkAndNotifyRecurringInvoices, checkAndNotifyRecurringInvoicesDaily } = require('./recurring');
 
 // Schedule the cron job to run at midnight on the first day of each month
 cron.schedule('0 0 1 * *', () => {
@@ -9,4 +10,18 @@ cron.schedule('0 0 1 * *', () => {
     timezone: 'UTC'
 });
 
-console.log('Cron job scheduled to send yearly summary on the first day of each month.'); 
+// New cron job: check for recurring invoices with next payment date within a week, daily at 1am UTC
+cron.schedule('0 1 * * *', () => {
+    checkAndNotifyRecurringInvoices().catch(console.error);
+}, {
+    scheduled: true,
+    timezone: 'UTC'
+});
+
+// Schedule the daily recurring invoice check to run at midnight every day
+cron.schedule('0 0 * * *', async () => {
+    console.log('Running daily recurring invoice check...');
+    await checkAndNotifyRecurringInvoicesDaily();
+});
+
+console.log('Cron jobs scheduled: yearly summary (monthly), recurring invoice check (daily).'); 
